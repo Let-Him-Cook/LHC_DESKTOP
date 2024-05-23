f3797cc7c9937d65b8105d3411123f3c93a5dd63
Victor H. Silva <victor.silvaPC@hotmail.com>
2024-05-22 23:57:30 -0300

    feat: :clown_face: add mocked authentication handler

---
+++ src/handlers/auth.py
@@ -0,0 +1,7 @@
+def authenticate(username, password):
+    valid_username = "admin"
+    valid_password = "admin"
+
+    if username == valid_username and password == valid_password:
+        return True
+    return False

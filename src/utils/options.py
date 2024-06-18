c2bc675102d8ea272a5655dabd81d027dde1a204
Victor H. Silva <victor.silvaPC@hotmail.com>
2024-06-18 15:10:41 -0300

    fix: fix the income and biggest sales charts to use menu options on api request

--- src/utils/options.py
+++ src/utils/options.py
@@ -22,3 +22,18 @@ MONTHS = [
   "Novembro", 
   "Dezembro"
 ]
+
+MONTHS_MAP = {
+    "Janeiro": 1,
+    "Fevereiro": 2,
+    "Março": 3,
+    "Abril": 4,
+    "Maio": 5,
+    "Junho": 6,
+    "Julho": 7,
+    "Agosto": 8,
+    "Setembro": 9,
+    "Outubro": 10,
+    "Novembro": 11,
+    "Dezembro": 12
+}
